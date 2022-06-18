58e395cfb7fdfd9be6f574ac724cbdaa33401594
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-06-18 22:52:47 +0100

    two sum first solution

--- javascript/1-Two-Sum.js
+++ javascript/1-Two-Sum.js
@@ -3,7 +3,20 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {};
+var twoSum = function (nums, target) {
+  let map = {};
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i];
+    let diff = target - num;
+    if (map[diff] !== undefined) {
+      return [map[diff], i];
+    }
+    map[num] = i;
+  }
+  return [];
+};
+
+console.log(twoSum([2, 7, 11, 15], 9));
 
 // Example 1:
 
