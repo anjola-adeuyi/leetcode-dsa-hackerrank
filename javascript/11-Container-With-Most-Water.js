5847a5a4f8e7702a385e2f06f2f26283b3d4794c
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-06-21 08:50:30 +0100

    algorithm maxArea

--- javascript/11-Container-With-Most-Water.js
+++ javascript/11-Container-With-Most-Water.js
@@ -3,4 +3,14 @@
  * @return {number}
  */
 
-var maxArea = function (height) {};
+var maxArea = function (height) {
+  let max = 0;
+  for (let i = 0; i < height.length; i++) {
+    for (let j = i + 1; j < height.length; j++) {
+      let area = Math.min(height[i], height[j]) * (j - i);
+      max = Math.max(max, area);
+    }
+  }
+
+  return max;
+};
