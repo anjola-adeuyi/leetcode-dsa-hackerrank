e91223c14dd8ef91fe2f9fa08a449285d1942200
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-06-22 23:35:05 +0100

    queue implementation

--- dsa/queue.js
+++ dsa/queue.js
@@ -27,3 +27,17 @@ class Queue {
     console.log(this.items.toString());
   }
 }
+
+const queue = new Queue();
+console.log(queue.isEmpty());
+
+queue.enqueue(20);
+queue.enqueue(10);
+queue.enqueue(30);
+
+console.log(queue.isEmpty());
+console.log(queue.size());
+queue.print();
+
+console.log(queue.dequeue());
+console.log(queue.peek());
