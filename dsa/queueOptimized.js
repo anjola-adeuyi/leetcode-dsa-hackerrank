a5089f7d7ee40da0fe10c5dda7992f46c0ea1824
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-06-22 23:52:30 +0100

    queue optimized defined

---
+++ dsa/queueOptimized.js
@@ -0,0 +1,41 @@
+class Queue {
+  constructor() {
+    this.items = {};
+    this.rear = 0;
+    this.front = 0;
+  }
+
+  enqueue(element) {
+    this.items[this.rear] = element;
+    this.rear++;
+  }
+
+  isEmpty() {
+    return this.rear === this.front;
+  }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    const tempItem = this.items[this.front];
+    delete this.items[this.front];
+    this.front++;
+    return tempItem;
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    return this.items[this.front];
+  }
+
+  size() {
+    return this.rear - this.front;
+  }
+
+  print() {
+    console.log(this.items);
+  }
+}
