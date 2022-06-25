8302d93ef996f6af1cdcbdea318091b97c8d1fd1
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-06-25 15:44:23 +0100

    optimize solution

--- javascript/49-Group-Anagrams.js
+++ javascript/49-Group-Anagrams.js
@@ -39,27 +39,27 @@ const CODES = {
  * @param {string[]} words
  * @return {string[][]}
  */
-function groupAnagrams(strs) {
-  // const hashMap = Object.create(null);
-  const hashMap = {};
+// function groupAnagrams(strs) {
+//   // const hashMap = Object.create(null);
+//   const hashMap = {};
 
-  for (let word of strs) {
-    const hashedWord = hashWord(word);
+//   for (let word of strs) {
+//     const hashedWord = hashWord(word);
 
-    if (!(hashedWord in hashMap)) {
-      hashMap[hashedWord] = [];
-    }
-    hashMap[hashedWord].push(word);
-  }
+//     if (!(hashedWord in hashMap)) {
+//       hashMap[hashedWord] = [];
+//     }
+//     hashMap[hashedWord].push(word);
+//   }
 
-  let groups = [];
+//   let groups = [];
 
-  for (let key in hashMap) {
-    groups.push(hashMap[key]);
-  }
+//   for (let key in hashMap) {
+//     groups.push(hashMap[key]);
+//   }
 
-  return groups;
-}
+//   return groups;
+// }
 
 /**
  * @param {string} word
@@ -88,24 +88,26 @@ function hashWord(word) {
  * @return {string[][]}
  */
 
-// const groupAnagrams = (strs) => {
-//   const result = [];
-//   const map = new Map();
-//   for (let i = 0; i < strs.length; i++) {
-//     const sorted = strs[i].split('').sort().join('');
-//     //! we are just splitting the string and sorting it and joining it back
-//     if (map.has(sorted)) {
-//       map.get(sorted).push(strs[i]); //! if the map has the sorted string, we push the string into the array
-//     } else {
-//       map.set(sorted, [strs[i]]); //! we are pushing the string into the map with the sorted string as the key
-//     }
-//   }
+const groupAnagrams = (strs) => {
+  const result = [];
+  const hashMap = new Map();
 
-//   for (let [key, value] of map) {
-//     result.push(value);
-//   }
-//   return result;
-// };
+  for (let i = 0; i < strs.length; i++) {
+    const sorted = strs[i].split('').sort().join(); //! we are just splitting the string and sorting it and joining it back
+
+    if (hashMap.has(sorted)) {
+      hashMap.get(sorted).push(strs[i]); //! if the map has the sorted string, we push the string into the array
+    } else {
+      hashMap.set(sorted, [strs[i]]); //! we are pushing the string into the map with the sorted string as the key for the first thing else condition
+    }
+  }
+
+  for (let [key, value] of hashMap) {
+    result.push(value);
+  }
+
+  return result;
+};
 
 module.exports = groupAnagrams;
 
