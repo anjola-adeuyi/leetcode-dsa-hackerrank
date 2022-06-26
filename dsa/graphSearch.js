bea8675928c4fae4e34640adaa86537b3d869bed
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-06-26 19:42:59 +0100

    adjacencyList

--- dsa/graphSearch.js
+++ dsa/graphSearch.js
@@ -4,7 +4,7 @@ const airports = 'PHX BKK OKC JFK LAX MEX EZE HEL LOS LAP LIM'.split(' ');
 console.log('airports:', airports);
 
 const routes = [
-  ['PKX', 'LAX'],
+  ['PHX', 'LAX'],
   ['PHX', 'JFK'],
   ['JFK', 'OKC'],
   ['JFK', 'HEL'],
@@ -17,4 +17,24 @@ const routes = [
 ];
 
 // The Graph
-const adjacencyList = {};
+const adjacencyList = new Map();
+
+console.log('adjacencyList:', adjacencyList);
+
+// Add node
+function addNode(airport) {
+  adjacencyList.set(airport, []);
+}
+
+// Add edge, undirected
+function addEdge(origin, destination) {
+  adjacencyList.get(origin).push(destination);
+  adjacencyList.get(destination).push(origin);
+}
+
+airports.forEach(addNode);
+routes.forEach(([origin, destination]) => addEdge(origin, destination));
+console.log('adjacencyList:', adjacencyList);
+
+// BFS Breadth First Search
+function bfs(start) {}
